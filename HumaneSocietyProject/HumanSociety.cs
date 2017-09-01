95881b33c7612e28624c2f327a29b1f22265a269
vangt <vangt86@gmail.com>
2017-09-01 13:15:13 -0500

    Made UserType method to ask if user is an adoptee or employee.

--- HumaneSocietyProject/HumanSociety.cs
+++ HumaneSocietyProject/HumanSociety.cs
@@ -8,7 +8,40 @@ namespace HumaneSocietyProject
 {
     public class HumanSociety
     {
-        DataClasses1DataContext database = new DataClasses1DataContext();
+        DataClasses1DataContext database;
+
+        public HumanSociety()
+        {
+            database = new DataClasses1DataContext();
+        }
+
+        public void UserType()
+        {
+            Console.WriteLine("Are you an adoptee or employee? \n 1: Adoptee \n 2: Employee");
+            string choice = Console.ReadLine();
+
+            switch(choice)
+            {
+                case "1":
+                    GetAdoptee();
+                    break;
+                case "2":
+                    GetEmployee();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void GetAdoptee()
+        {
+
+        }
+
+        public void GetEmployee()
+        {
+
+        }
 
         public void ListOfAnimals()
         {
@@ -19,8 +52,6 @@ namespace HumaneSocietyProject
             {
                 Console.WriteLine(c.Name);
             }
-            Console.ReadLine();
         }
-        
     }
 }
