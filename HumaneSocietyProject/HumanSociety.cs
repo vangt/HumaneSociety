f78a85acc99be8138b279010bc0943bfc033951d
vangt <vangt86@gmail.com>
2017-09-01 13:37:45 -0500

    Made a method to grab new user info and login user info.

--- HumaneSocietyProject/HumanSociety.cs
+++ HumaneSocietyProject/HumanSociety.cs
@@ -23,19 +23,82 @@ namespace HumaneSocietyProject
             switch(choice)
             {
                 case "1":
-                    GetAdoptee();
+                    Console.Clear();
+                    GetCheckAdoptee();
                     break;
                 case "2":
+                    Console.Clear();
                     GetEmployee();
                     break;
                 default:
+                    Console.Clear();
+                    Console.WriteLine("You've inputed an incorrect option.");
+                    UserType();
                     break;
             }
         }
 
-        public void GetAdoptee()
+        public void GetCheckAdoptee()
         {
+            Console.WriteLine("Are you a new user? \n 1: Yes, I'm new and have don't have an account. \n 2: No, I'm an existing user and have an account.");
+            string choice = Console.ReadLine();
 
+            switch(choice)
+            {
+                case "1":
+                    Console.Clear();
+                    Register();
+                    break;
+                case "2":
+                    Console.Clear();
+                    GetAdopteeLogin();
+                    break;
+                default:
+                    Console.WriteLine("You choose ");
+                    break;
+            }
+        }
+
+        public void Register()
+        {
+            Console.WriteLine("Please enter your first name.");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name.");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter just your street adress.");
+            string streetAddress = Console.ReadLine();
+
+            Console.WriteLine("Please enter your city name.");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Please enter your state initials.");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Please enter your 5-digit zip code.");
+            string zip = Console.ReadLine();
+
+            Console.WriteLine("Please enter your phone.");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("Please enter your date of birth.");
+            string dob = Console.ReadLine();
+
+            Console.WriteLine("Please enter your Username.");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password.");
+            string password = Console.ReadLine();
+        }
+
+        public void GetAdopteeLogin()
+        {
+            Console.WriteLine("Please enter your user name.");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password.");
+            string password = Console.ReadLine();
         }
 
         public void GetEmployee()
